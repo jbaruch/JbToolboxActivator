93dfc38f89cb57ce5c108b7b958cd3d8e4732972
jbaruch <jbaruch@jfrog.com>
2017-03-15 03:04:20 -0700

    proper http response is now sent

--- jbToolboxActivator.groovy
+++ jbToolboxActivator.groovy
@@ -3,11 +3,13 @@
   @Grab('org.slf4j:slf4j-simple:1.7.21')
 ])
 import static ratpack.groovy.Groovy.ratpack
+import ratpack.exec.*
 
 ratpack {
     handlers {
         get(':product') {
-          "open jetbrains://${pathTokens.product}/".execute()
+          Thread.start {"open jetbrains://${pathTokens.product}/".execute()}
+          response.send()
         }
     }
 }
